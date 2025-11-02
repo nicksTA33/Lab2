a99ab41f7c9b733110024656223fe91e506c3c05
nick <slupskiy.m.06@gmail.com>
2025-11-02 12:51:34 +0200

    Added test for withdraw wrong currency

--- BankAccountTests/BankAccount.tests/BankAccountTests.cs
+++ BankAccountTests/BankAccount.tests/BankAccountTests.cs
@@ -1,7 +1,12 @@
+
+using System.ComponentModel;
+
 namespace BankAccount.tests
 {
     public class BankAccountTests
     {
+        BankAccount _account = new BankAccount(new Money { Amount = 10, Currency = Currency.EUR });
+
         [Fact]
         public void Withdraw_WithdrawingMoreThanAvailable_ThrowsInvalidOperation()
         {
@@ -11,5 +16,14 @@ namespace BankAccount.tests
             Assert.Throws<InvalidOperationException>(() => account.Withdraw(amount));
            
         }
+
+        [Fact]
+
+        public void Withdraw_WithdrawWrongCurrency_ThrowsInvalidOperation()
+        {
+            var amount = new Money { Amount = 5, Currency = Currency.UAH };
+            Assert.Throws<InvalidEnumArgumentException>(() => _account.Withdraw(amount));
+        }
+
     }
 }
