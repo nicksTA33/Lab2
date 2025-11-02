32b9946c6835d9127c2b71b523e35e1dbc3e8ec0
DimaOliinyk <Oliinyk.Dmytro2@lll.kpi.ua>
2025-11-02 12:51:21 +0200

    Added test for Replenish wrong currency

--- BankAccountTests/BankAccount.tests/BankAccountTests.cs
+++ BankAccountTests/BankAccount.tests/BankAccountTests.cs
@@ -1,17 +1,24 @@
-using System.Security.Principal;
+using System.ComponentModel;
 
 namespace BankAccount.tests;
 
 public class BankAccountTests
 {
+    private BankAccount _account = new(new Money{ Amount=10, Currency=Currency.USD });
+
     [Theory]
     [InlineData(1, 11)]
     [InlineData(2, 12)]
     public void Replenish_BalanceIncreases_Correctly(decimal moneyToAdd, decimal excpectedBalance)
     {
-        var account = new BankAccount(new Money{Amount=10, Currency=Currency.USD });
+        _account.Replenish(new Money { Amount = moneyToAdd, Currency=Currency.USD });
+        Assert.Equal(_account.Balance.Amount, excpectedBalance);
+    }
 
-        account.Replenish(new Money { Amount = moneyToAdd, Currency=Currency.USD });
-        Assert.Equal(account.Balance.Amount, excpectedBalance);
+    [Fact]
+    public void Replenish_WrongCurrency_ThrowsInvalidOperation()
+    {
+        var amount = new Money() { Amount=5, Currency=Currency.EUR };
+        Assert.Throws<InvalidEnumArgumentException>(() => _account.Replenish(amount));
     }
 }
